2c7e004ef82fe10707229618088336477bd86e69
ermias kidane <hadeklte21@gmail.com>
2019-09-01 13:40:13 +0100

    initiate commit

---
+++ controllers/auth.js
@@ -0,0 +1,162 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
+
+const crypto = require("crypto");
+
+const transporter = nodemailer.createTransport(
+  sendGridTransport({
+    auth: {
+      api_key: "API_KEY"
+    }
+  })
+);
+
+exports.getLogin = (req, res, next) => {
+  // console.log(req.flash("error"));
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: message
+    // errorMessage: req.flash("error")
+    // isAuthenticated: false coz we add to a middleware in app.js
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: message
+    // isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        req.flash("error", "Invalid email or password");
+        return res.redirect("/login");
+      }
+
+      // compare the password
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          req.flash("error", "Invalid email or password ");
+          res.redirect("/login");
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect("/login");
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  // check if user exist
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        // console.log("user already exist");
+        req.flash(
+          "error",
+          "E-mail exists already, please pick a different one"
+        );
+        return res.redirect("/signUp");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            name: "Eri",
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: "shop@node-compelte.com",
+            subject: "Singup succeded",
+            html: "<h1>you succesfully signup!</h1>"
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      // const user = new User({
+      //   name: "Eri",
+      //   email: email,
+      //   password: password,
+      //   cart: { items: [] }
+      // });
+      // return user.save();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+  });
+};
