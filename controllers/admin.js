87ef36a58e9b24111f791f796e210dd47ec6297e
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2019-09-01 13:44:25 +0100

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -55,7 +55,6 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product
-        // isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
