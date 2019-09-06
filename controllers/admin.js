a26ac6b86ccefbcfdaab3027b881c282acd255bd
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2019-09-06 01:32:25 +0100

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,25 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
 
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    });
+  }
   const product = new Product({
     title: title,
     price: price,
