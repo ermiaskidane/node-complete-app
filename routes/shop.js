68f2bdbe88ed8c4569c272730548e7b35143a285
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2019-10-13 15:38:50 +0100

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -20,9 +20,18 @@ router.post("/cart", isAuth, shopController.postCart);
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
 router.get("/checkout", isAuth, shopController.getCheckout);
+// // since stripe doesn't need csrf token protection we need to excluded
+// // by placing before csrf token generate in app.js
+// // router.post("/create-order", isAuth, shopController.postOrder);
 
-router.post("/create-order", isAuth, shopController.postOrder);
+// supposed to work for the updated method but does not, to work with
+// old method do not forget to install the old version of stripe
+// router.get("/checkout/success", shopController.getCheckoutSuccess);
+// router.get("/checkout/cancel", shopController.getCheckout);
 
 router.get("/orders", isAuth, shopController.getOrders);
 
+router.get("/orders/:orderId", isAuth, shopController.getInvoice);
+
 module.exports = router;
+
