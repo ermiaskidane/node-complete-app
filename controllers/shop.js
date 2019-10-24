5de7670104b9651cb06b3388b7a3ad8a01f35e85
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2019-10-24 16:55:48 +0100

    Update shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,17 +59,52 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  // if query not hold a value, show 1
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
+
+  // Product.find()
+  //   .skip((page - 1) * ITEMS_PER_PAGE)
+  //   .limit(ITEMS_PER_PAGE)
+  //   .then(products => {
+  //     res.render("shop/index", {
+  //       prods: products,
+  //       pageTitle: "Shop",
+  //       path: "/"
+  //     });
+  //   })
+  //   .catch(err => {
+  //     const error = new Error(err);
+  //     error.httpStatusCode = 500;
+  //     return next(error);
+  //   });
 };
 
 exports.getCart = (req, res, next) => {
